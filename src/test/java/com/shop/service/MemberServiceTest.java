f318defc313afb29db0eafd826a0978c898bf755
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-09-07 17:55:13 +0900

    P157 (Thymeleaf Test,header,footer,security 테스트 )

---
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package com.shop.service;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("안산시 상록구 본오2동");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest(){
+        Member member = createMember();
+        Member saveMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(),saveMember.getEmail());
+        assertEquals(member.getName(),saveMember.getName());
+        assertEquals(member.getAddress(),saveMember.getAddress());
+        assertEquals(member.getPassword(),saveMember.getPassword());
+        assertEquals(member.getRole(),saveMember.getRole());
+    }
+}
